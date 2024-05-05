8a4869ad6200e1e0e184aaec4dc79f146cf14f97
Soumencsit <sp01csit@gmail.com>
2024-05-05 09:51:20 +0530

    second commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,13 +10,7 @@ function Home(){
                     </div>
 
                     <div></div>
-                    {/* <a href="#_" className="relative inline-flex items-center px-5 py-3 overflow-hidden text-lg font-medium text-sky-600 border-2 border-sky-600 rounded-full hover:text-white group hover:bg-gray-50">
-                    <span className="absolute left-0 block w-full h-0 transition-all bg-sky-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-                    <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                    </span>
-                    <span className="relative">SignUp</span>
-                    </a> */}
+                    
                     <button className=' h-12 w-3/5 rounded-md  bg-gradient-to-r from-sky-400 to-sky-600 hover:from-sky-500 hover:to-sky-800'>SignUp</button>
                 </div>
               
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -26,7 +26,7 @@ function Nav(){
                             <a href="#" className="block py-2 px-3 md:p-0 text-white bg-sky-700 rounded md:bg-transparent md:text-sky-700 md:dark:text-sky-500" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-700 md:dark:hover:text-sky-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-700 md:dark:hover:text-sky-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Abouttt</a>
                         </li>
                         <li>
                             <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-700 md:dark:hover:text-sky-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
